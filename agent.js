d74132cb6085133062048c6d8dbce3a0738ba334
alex zhang <gzane24@gmail.com>
2018-03-04 02:03:12 +0800

    fix dir path of apidoc bin

--- agent.js
+++ agent.js
@@ -7,7 +7,7 @@ module.exports = app => {
 
   if (config.env === 'local') {
     // apidoc -i app/controller -o app/public/apidoc
-    const apiDocPath = path.join(__dirname, 'node_modules/.bin/apidoc');
+    const apiDocPath = path.join(app.baseDir, 'node_modules/.bin/apidoc');
     const apiSourcePath = path.join(app.baseDir, 'app/controller');
     const apiTargetPath = path.join(app.baseDir, 'app/public/apidoc');
 
