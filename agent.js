88e5b22372c3294f249a77ab7219979883b42067
alex zhang <gzane24@gmail.com>
2018-03-04 01:48:15 +0800

    fix log

--- agent.js
+++ agent.js
@@ -3,14 +3,10 @@
 const path = require('path');
 
 module.exports = app => {
-  const { logger, config } = app;
+  const { config } = app;
 
   if (config.env === 'local') {
-    logger.info('local 环境 开始生成doc api');
-
-    // 只有开发环境使用
     // apidoc -i app/controller -o app/public/apidoc
-
     const apiDocPath = path.join(__dirname, 'node_modules/.bin/apidoc');
     const apiSourcePath = path.join(app.baseDir, 'app/controller');
     const apiTargetPath = path.join(app.baseDir, 'app/public/apidoc');
@@ -18,7 +14,7 @@ module.exports = app => {
     require('shelljs').exec(`${apiDocPath} -i ${apiSourcePath} -o ${apiTargetPath}`,
       { async: true, silent: true },
       (code, stdout, stderr) => {
-        logger.info(`local 环境 生成doc api ${stdout} stderr:${stderr}`);
+        app.coreLogger.info(`local 环境 生成doc api ${stdout} stderr:${stderr}`);
       });
   }
 };
